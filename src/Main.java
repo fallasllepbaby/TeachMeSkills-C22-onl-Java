b7f4523ae46bcb519f1113db4785b4e9365a40a3
Laparevich Daniil <danik.laparevich@bk.ru>
2023-05-20 16:36:22 +0300

    add: method to return scanner
    add: additional task about months

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        getData();
-        convertCharToInt();
-        countBMI();
+//        getData();
+//        convertCharToInt();
+//        countBMI();
+        getMonthByNumber();
     }
 
     public static void getData(){
@@ -53,5 +54,65 @@ public class Main {
 
         float bmi = (float) (weight / Math.pow(height / 100, 2));
         System.out.print("BMI = " + String.format("%.2f", bmi));
+    }
+
+    // Method to return scanner
+    public static Scanner scan(){
+        return new Scanner(System.in);
+    }
+
+    // Additional task
+    public static void getMonthByNumber() {
+        int month = 0;
+        boolean check = true;
+        /*
+        do {
+            System.out.print("Enter number of month: ");
+            month = scan().nextInt();
+        } while (!(month > 0 && month < 13));
+        */
+        System.out.print("Enter number of month: ");
+        month = scan().nextInt();
+
+        switch (month){
+            case 1:
+                System.out.println("January");
+                break;
+            case 2:
+                System.out.println("February");
+                break;
+            case 3:
+                System.out.println("March");
+                break;
+            case 4:
+                System.out.println("April");
+                break;
+            case 5:
+                System.out.println("May");
+                break;
+            case 6:
+                System.out.println("June");
+                break;
+            case 7:
+                System.out.println("July");
+                break;
+            case 8:
+                System.out.println("August");
+                break;
+            case 9:
+                System.out.println("September");
+                break;
+            case 10:
+                System.out.println("October");
+                break;
+            case 11:
+                System.out.println("November");
+                break;
+            case 12:
+                System.out.println("December");
+                break;
+            default:
+                System.out.println("There is no such month in the year!");
+        }
     }
 }
