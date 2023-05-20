9f0510a08c9eed3ebad11b4a7394304d85a97e8b
Laparevich Daniil <danik.laparevich@bk.ru>
2023-05-20 16:40:06 +0300

    delete: previous task from homework3_additional branch

--- src/Main.java
+++ src/Main.java
@@ -3,57 +3,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-//        getData();
-//        convertCharToInt();
-//        countBMI();
         getMonthByNumber();
-    }
-
-    public static void getData(){
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Enter your name: ");
-        String name = scanner.nextLine();
-
-        System.out.print("Enter your surname: ");
-        String surname = scanner.nextLine();
-
-        System.out.print("Enter your age: ");
-        int age = scanner.nextInt();
-
-        System.out.print("Enter your height: ");
-        int height = scanner.nextInt();
-
-        System.out.print("Enter your weight: ");
-        float weight = scanner.nextFloat();
-
-        System.out.println("Name: " + name);
-        System.out.println("Surname: " + surname);
-        System.out.println("Age: " + age);
-        System.out.println("Height: " + height);
-        System.out.println("Weight: " + weight);
-    }
-
-    public static void convertCharToInt(){
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Enter char: ");
-        char symbol = scanner.next().charAt(0);
-        System.out.println(symbol + "-" + (int) symbol);
-    }
-
-    // BMI (Body Mass Index)
-    public static void countBMI(){
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Enter your weight: ");
-        float weight = scanner.nextFloat();
-
-        System.out.print("Enter your height: ");
-        float height = scanner.nextFloat();
-
-        float bmi = (float) (weight / Math.pow(height / 100, 2));
-        System.out.print("BMI = " + String.format("%.2f", bmi));
     }
 
     // Method to return scanner
